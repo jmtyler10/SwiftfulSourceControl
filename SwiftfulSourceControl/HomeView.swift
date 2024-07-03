2047acc7c84725ebf9633a45795b0f3f720ea431
Tyler <jmtyler10@gmail.com>
2024-07-03 09:35:08 -0400

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Tyler Martin on 7/3/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
