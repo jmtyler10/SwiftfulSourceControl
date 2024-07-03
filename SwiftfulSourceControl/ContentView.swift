cf070b63369eeb5943b6ed8c7346177499cba056
Tyler <jmtyler10@gmail.com>
2024-07-03 13:53:59 -0400

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Tyler Martin on 7/2/24.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] Description of bug
+ 
+ MUNDANE TASK
+ [Clean] Description of changes
+ 
+ RELEASE
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
